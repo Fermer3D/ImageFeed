e5b59450a25e026bae7c24b4eb9e384a154da203
Fermer3D <danil.tretyachenko.03@yandex.ru>
2026-01-31 01:18:35 +0300

    Fix review comments

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -12,5 +12,5 @@ enum Constants {
     static let secretKey = "2yK-qYk0rzLAVHHM_dkRPpeJtTR6V_FeBbXAD2xBiEo"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
 }
